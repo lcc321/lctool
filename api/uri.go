d431286562676dc5f93b62f1944f2a93b474a789
lichichuang <lichichuang@lichichuangdeMacBook-Pro.local>
2021-09-24 18:47:47 +0800

    update

---
+++ api/uri.go
@@ -0,0 +1,11 @@
+package api
+
+var (
+	Base = "https://leetcode-cn.com/"
+	Login = "https://leetcode-cn.com/accounts/login/"
+	Graphql = "https://leetcode-cn.com/graphql"
+	ProblemsAll = "https://leetcode-cn.com/api/problems/all/"
+	Problem = "https://leetcode-cn.com/problems/$slug"
+	Submit = "https://leetcode-cn.com/problems/$slug/submit/"
+	submission = "https://leetcode-cn.com/submissions/detail/$id/"
+)
